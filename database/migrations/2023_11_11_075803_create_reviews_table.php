079049c25aaf7809a8e266ac9f9dedf6aa50b5e1
Arkar03 <arkarmin.mi@gmail.com>
2023-11-12 16:14:58 +0630

    chore: eloquent relation added

--- database/migrations/2023_11_11_075803_create_reviews_table.php
+++ database/migrations/2023_11_11_075803_create_reviews_table.php
@@ -13,8 +13,10 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
-            $table->integer('book_id');
-            $table->integer('user_id');
+            $table->unsignedBigInteger('book_id');
+            $table->foreign('book_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->text('review');
             $table->timestamps();
         });
